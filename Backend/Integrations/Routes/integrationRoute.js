be8b4b3d32505a0bafa81cdec4bb95a0d817bdda
Atanu Mondal <atanu.mondal.me48@gmail.com>
2022-07-21 22:27:16 +0530

    Integration backend completed

--- Backend/Integrations/Routes/integrationRoute.js
+++ Backend/Integrations/Routes/integrationRoute.js
@@ -2,9 +2,28 @@ const { Router } = require("express");
 const IntegrationModel = require("../Model/Integration");
 const integrationRouter = Router();
 
-integrationRouter.get("/", (req, res) => {
-  res.send(
-    "Welcom to integration home url. Use '/integrations' to get inegration data"
-  );
+
+
+integrationRouter.get("/", async (req, res) => {
+  let integrations = await IntegrationModel.find({}, { __v: 0 });
+  res.send(integrations);
 });
 
+integrationRouter.get("/:key", async (req, res) => {
+  let data = await IntegrationModel.find({
+    $or: [{ title: { $regex: req.params.key } }],
+  });
+  res.send(data);
+});
+
+integrationRouter.post("/", async (req, res) => {
+  // console.log(req.body.data)
+  let integrations;
+  req.body.data.map((el) => {
+    integrations = new IntegrationModel(el);
+    integrations.save();
+  });
+  res.send(integrations);
+});
+
+module.exports = integrationRouter;
