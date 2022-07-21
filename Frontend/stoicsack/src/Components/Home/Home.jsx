13975564c09fe5ad027d59ef6a8f863d6dcc41a4
skashyap061998 <skashyap061998@gmail.com>
2022-07-21 22:13:11 +0530

    day3 part 1

--- Frontend/stoicsack/src/Components/Home/Home.jsx
+++ Frontend/stoicsack/src/Components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
 import styles from "./Home.module.css";
 const Home = () => {
   const [skInput1, setSkInput1] = useState("");
+
+  
   return (
     <>
+    <br />
       <Navbar />
 
       <br />
@@ -23,7 +27,7 @@ const Home = () => {
         </div>
         <div className={styles.ska12}>
           <input
-          className={styles.ska121}
+            className={styles.ska121}
             type="text"
             value={skInput1}
             onChange={(e) => setSkInput1(e.target.value)}
@@ -32,6 +36,26 @@ const Home = () => {
           <button className={styles.ska122}>START FOR FREE</button>
         </div>
         <p className={styles.ska123}>14-day PRO trial included</p>
+        <div className={styles.ska124}>
+          <img
+          className={styles.ska125}
+            src="https://trackingtime.co/wp-content/themes/trackingtime-v4/img/keywords/google-key.png"
+            alt=""
+          />
+          <Link to=""><p className={styles.ska126}>Sing in with Google</p></Link>
+        </div>
+      </div>
+
+{/* 2nd div start from here */}
+
+      <div className={styles.ska2}>
+        <br /><br /><br />
+      <p className={styles.ska21}>Companies of all shapes and sizes use TrackingTime:</p>
+<div className={styles.ska22}></div>
+      </div>
+
+      {/* 3 part starts from herer */}
+      <div style={{height:"40vh"}}>
       </div>
     </>
   );
