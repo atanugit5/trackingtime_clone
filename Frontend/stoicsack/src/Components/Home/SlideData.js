38779e6b820d8c4ebc83450066f675e359902436
skashyap061998 <skashyap061998@gmail.com>
2022-07-22 10:03:52 +0530

    day3 done

---
+++ Frontend/stoicsack/src/Components/Home/SlideData.js
@@ -0,0 +1,45 @@
+export const SliderData = [
+    {
+        id:1,
+        pic:"https://trackingtime.co/wp-content/themes/trackingtime-v4/img/home/logos/color/logo2.jpg"
+    },
+    {
+        id:2,
+        pic:"https://trackingtime.co/wp-content/themes/trackingtime-v4/img/home/logos/color/logo3.jpg"
+    },
+    {
+        id:3,
+        pic:"https://trackingtime.co/wp-content/themes/trackingtime-v4/img/home/logos/color/logo4.jpg"
+    },
+    {
+        id:4,
+        pic:"https://trackingtime.co/wp-content/themes/trackingtime-v4/img/home/logos/color/logo5.jpg"
+    },
+    {
+        id:5,
+        pic:"https://trackingtime.co/wp-content/themes/trackingtime-v4/img/home/logos/color/logo6.jpg"
+    },
+    {
+        id:6,
+        pic:"https://trackingtime.co/wp-content/themes/trackingtime-v4/img/home/logos/color/logo7.jpg"
+    },
+    {
+        id:7,
+        pic:"https://trackingtime.co/wp-content/themes/trackingtime-v4/img/home/logos/color/logo8.jpg"
+    },
+    // {
+    //     id:8,
+    //     pic:"https://trackingtime.co/wp-content/themes/trackingtime-v4/img/home/logos/color/logo9.jpg"
+    // },
+    // {
+    //     id:9,
+    //     pic:"https://trackingtime.co/wp-content/themes/trackingtime-v4/img/home/logos/color/logo10.jpg"
+    // },
+    // {
+    //     id:10,
+    //     pic:"https://trackingtime.co/wp-content/themes/trackingtime-v4/img/home/logos/color/logo11.jpg"
+    // },
+
+    
+
+]
