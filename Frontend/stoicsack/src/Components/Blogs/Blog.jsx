97bbf46e4de7b879110a085732a80101853b7055
Shivaji z <99667776+CodEsHiVaz@users.noreply.github.com>
2022-07-22 21:23:15 +0530

    day4 update

--- Frontend/stoicsack/src/Components/Blogs/Blog.jsx
+++ Frontend/stoicsack/src/Components/Blogs/Blog.jsx
@@ -2,81 +2,61 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styles from "./Blog.module.css";
 import { Blogcat } from "./Blogcat";
-import { useParams } from "react-router-dom";
-// import { Blogcat } from "./Blogcat";
+import { useParams, useNavigate } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 export const Blog = () => {
+  const navfunc = useNavigate();
   const { category } = useParams();
-  console.log(`🚀 ~ Blog ~ getparams`, category);
-  const [liActive1, setLiActive1] = useState(true);
-  const [liActive2, setLiActive2] = useState(false);
-  const [liActive3, setLiActive3] = useState(false);
   const [sitedata, setsitedata] = useState([]);
-
-  const focouse_controler1 = () => {
-    setLiActive1(true);
-    setLiActive2(false);
-    setLiActive3(false);
-    // setsitedata()
-    axios
-      .get(`https://blogapi-for-stoic3017.herokuapp.com/blogs/${category}`)
-      .then((r) => setsitedata(r.data));
+  const [liActive, setLiActive] = useState(category);
+  const focouse_controler = (cat) => {
+    setLiActive(cat);
+    navfunc(`/blog/${cat}`);
   };
-
-  const focouse_controler2 = () => {
-    setLiActive1(false);
-    setLiActive2(true);
-    setLiActive3(false);
-    axios
-      .get(`https://blogapi-for-stoic3017.herokuapp.com/blogs/${category}`)
-      .then((r) => setsitedata(r.data));
-  };
-  const focouse_controler3 = () => {
-    setLiActive1(false);
-    setLiActive2(false);
-    setLiActive3(true);
-    axios
-      .get(`https://blogapi-for-stoic3017.herokuapp.com/blogs/${category}`)
-      .then((r) => setsitedata(r.data));
-  };
-
   useEffect(() => {
     axios
       .get(`https://blogapi-for-stoic3017.herokuapp.com/blogs/${category}`)
       .then((r) => setsitedata(r.data));
-  }, [liActive1, liActive2, liActive3]);
-
+  }, [liActive]);
   return (
-    <div className={styles.main_container}>
-      <ul className={styles.list_Container}>
-        <li
-          className={`${styles.list_element} ${
-            liActive1 ? styles.activetab : ""
-          }`}
-          onClick={focouse_controler1}
-        >
-          blog
-        </li>
-        <li
-          className={`${styles.list_element} ${
-            liActive2 ? styles.activetab : ""
-          }`}
-          onClick={focouse_controler2}
-        >
-          productivity
-        </li>
-        <li className={styles.list_element}>remote work</li>
-        <li
-          className={`${styles.list_element} ${
-            liActive3 ? styles.activetab : ""
-          }`}
-          onClick={focouse_controler3}
-        >
-          best practices
-        </li>
-        <li className={styles.list_element}>resources</li>
-      </ul>
-      <Blogcat category={sitedata} />
-    </div>
+    <>
+      <div className={styles.main_container}>
+        <br />
+        <Navbar />
+        <br />
+
+        <ul className={styles.list_Container}>
+          <li
+            className={`${styles.list_element} ${
+              liActive === "blogs" ? styles.activetab : ""
+            }`}
+            onClick={() => focouse_controler("blogs")}
+          >
+            blog
+          </li>
+          <li
+            className={`${styles.list_element} ${
+              liActive === "productivity" ? styles.activetab : ""
+            }`}
+            onClick={() => focouse_controler("productivity")}
+          >
+            productivity
+          </li>
+          <li className={styles.list_element}>remote work</li>
+          <li
+            className={`${styles.list_element} ${
+              liActive === "best_practicess" ? styles.activetab : ""
+            }`}
+            onClick={() => focouse_controler("best_practicess")}
+          >
+            best practices
+          </li>
+          <li className={styles.list_element}>resources</li>
+        </ul>
+
+        <Blogcat category={sitedata} />
+      </div>
+    </>
   );
 };
--- Frontend/stoicsack/src/Components/Blogs/Blogcat.jsx
+++ Frontend/stoicsack/src/Components/Blogs/Blogcat.jsx
@@ -2,13 +2,31 @@ import React from "react";
 import styles from "./Blog.module.css";
 export const Blogcat = ({ category }) => {
   return (
-    <div className={styles.grid_structured}>
-      {category.map((elem) => (
-        <div key={elem._id}>
-          <img src={elem.thumburl} alt={elem.thumburl} />
-          <h3>{elem.title}</h3>
-        </div>
-      ))}
+    <div>
+      <div className={styles.top_article}>
+        <p>TIMEKEEPING AND PRODUCTIVITY ARTICLES</p>
+        <img
+          src="https://trackingtime.co/wp-content/uploads/2022/07/Solve-Your-Freelancing-Productivity-and-Budget-Problems.svg"
+          alt="How Time Tracking Can Solve Your Freelancing Productivity and Budget Problems"
+        />
+        <h1>
+          How Time Tracking Can Solve Your Freelancing Productivity and Budget
+          Problems
+        </h1>
+        <button className={styles.top_articale}>read article</button>
+      </div>
+      <div className={styles.grid_structured}>
+        {category.map((elem) => (
+          <div key={elem._id} className={styles.grid_individ_child}>
+            <div>
+              <img src={elem.thumburl} alt={elem.thumburl} />
+            </div>
+            <div>
+              <h3>{elem.title}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
