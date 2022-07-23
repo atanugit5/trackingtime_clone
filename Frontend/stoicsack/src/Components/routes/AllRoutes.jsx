186cee0f4ba6b4d64ed96121af7a708e2f65df0d
Atanu Mondal <atanu.mondal.me48@gmail.com>
2022-07-23 09:51:06 +0530

    integration single page structure done

--- Frontend/stoicsack/src/Components/routes/AllRoutes.jsx
+++ Frontend/stoicsack/src/Components/routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Blog } from "../Blogs/Blog";
 import Home from "../Home/Home";
+import { Integration } from "../Integrations/Integration";
 import { Integrations } from "../Integrations/Integrations";
 import { Login } from "../Login/Login";
 import Managament from "../Managament/Managament";
@@ -15,6 +16,7 @@ export const AllRoutes = () => {
         <Route path="/project-management" element={<Managament />} />
         <Route path="/time-cards" element={<Timecards/>}/>
         <Route path="/integrations" element={<Integrations/>}/>
+        <Route path="/integrations/:id" element={<Integration/>}/>
         <Route path="/blog/:category" element={<Blog/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
