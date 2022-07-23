4421dd03df6e0649e719517aefc13871c940e761
skashyap061998 <skashyap061998@gmail.com>
2022-07-23 10:15:40 +0530

    day 4

--- Frontend/stoicsack/src/Components/Home/Home.jsx
+++ Frontend/stoicsack/src/Components/Home/Home.jsx
@@ -309,76 +309,92 @@ const Home = () => {
             TrackingTime to simplify their timesheets.
           </p>
           <div className={styles.ska621}>
-          <input
-            className={styles.ska622}
-            type="text"
-            value={skInput1}
-            onChange={(e) => setSkInput1(e.target.value)}
-            placeholder="Your Work Email"
-          />
-          <button className={styles.ska623}>START FOR FREE</button>
-        </div>
-        <p className={styles.ska624} style={{color:"black",fontSize:"0.8rem",fontWeight:"550",marginTop:"0.9vh"}}>14-day PRO trial included</p>
-        <div className={styles.ska625}>
-          <img
-            className={styles.ska626}
-            src="https://trackingtime.co/wp-content/themes/trackingtime-v4/img/keywords/google-key.png"
-            alt=""
-          />
-          <Link to="">
-            <p className={styles.ska627}>Sing in with Google</p>
-          </Link>
-        </div>
+            <input
+              className={styles.ska622}
+              type="text"
+              value={skInput1}
+              onChange={(e) => setSkInput1(e.target.value)}
+              placeholder="Your Work Email"
+            />
+            <button className={styles.ska623}>START FOR FREE</button>
+          </div>
+          <p
+            className={styles.ska624}
+            style={{
+              color: "black",
+              fontSize: "0.8rem",
+              fontWeight: "550",
+              marginTop: "0.9vh",
+            }}
+          >
+            14-day PRO trial included
+          </p>
+          <div className={styles.ska625}>
+            <img
+              className={styles.ska626}
+              src="https://trackingtime.co/wp-content/themes/trackingtime-v4/img/keywords/google-key.png"
+              alt=""
+            />
+            <Link to="">
+              <p className={styles.ska627}>Sing in with Google</p>
+            </Link>
+          </div>
         </div>
       </div>
 
-
-
       {/* Footer part */}
 
-
-
-      <div  className={styles.ska7}>
+      <div className={styles.ska7}>
         <div style={{ height: "35vh" }}></div>
-          <div className={styles.ska71}>
-
-        <div className={styles.ska62}>
-          <img src="https://trackingtime.co/wp-content/themes/trackingtime-v4/img/layout/footer/tt.svg" alt="" />
-          <h1>Get Started</h1>
-          <p>
-            Sign up today and join thousands of people around the world using
-            TrackingTime to simplify their timesheets.
-          </p>
-          <div className={styles.ska621}>
-          <input
-            className={styles.ska622}
-            type="text"
-            value={skInput1}
-            onChange={(e) => setSkInput1(e.target.value)}
-            placeholder="Your Work Email"
-          />
-          <button className={styles.ska623}>START FOR FREE</button>
-        </div>
-        <p className={styles.ska624} style={{color:"black",fontSize:"0.8rem",fontWeight:"550",marginTop:"0.9vh"}}>14-day PRO trial included</p>
-        <div className={styles.ska625}>
-          <img
-            className={styles.ska626}
-            src="https://trackingtime.co/wp-content/themes/trackingtime-v4/img/keywords/google-key.png"
-            alt=""
+        <div className={styles.ska71}>
+          <div className={styles.ska62}>
+            <img
+              src="https://trackingtime.co/wp-content/themes/trackingtime-v4/img/layout/footer/tt.svg"
+              alt=""
             />
-          <Link to="">
-            <p className={styles.ska627}>Sing in with Google</p>
-          </Link>
-        </div>
-        </div>
+            <h1>Get Started</h1>
+            <p>
+              Sign up today and join thousands of people around the world using
+              TrackingTime to simplify their timesheets.
+            </p>
+            <div className={styles.ska621}>
+              <input
+                className={styles.ska622}
+                type="text"
+                value={skInput1}
+                onChange={(e) => setSkInput1(e.target.value)}
+                placeholder="Your Work Email"
+              />
+              <button className={styles.ska623}>START FOR FREE</button>
             </div>
+            <p
+              className={styles.ska624}
+              style={{
+                color: "black",
+                fontSize: "0.8rem",
+                fontWeight: "550",
+                marginTop: "0.9vh",
+              }}
+            >
+              14-day PRO trial included
+            </p>
+            <div className={styles.ska625}>
+              <img
+                className={styles.ska626}
+                src="https://trackingtime.co/wp-content/themes/trackingtime-v4/img/keywords/google-key.png"
+                alt=""
+              />
+              <Link to="">
+                <p className={styles.ska627}>Sing in with Google</p>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* real footer */}
 
-
-      <div className={styles.ska8}>
-
+      <div className={styles.ska8}> 
       </div>
     </>
   );
